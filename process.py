a7029035abcf2a27d53e67ac808df716f371ed35
Megan Williams <wilm140@ucla.edu>
2022-11-26 20:15:22 -0700

    Allow old files to be overwritten automatically

--- process.py
+++ process.py
@@ -1,9 +1,9 @@
 import os
 import subprocess
-video_dir = '/home/pi/Desktop/videos/'
-out_dir = '/home/pi/Desktop/processed/'
+video_dir = '/home/pi/Desktop/motion-sensor/videos/'
+out_dir = '/home/pi/Desktop/motion-sensor/processed/'
 videos = os.listdir(video_dir)
 for i in range(len(videos)):
     f = video_dir + videos[i]
     out_file = out_dir + 'video' + str(i) + '.mkv'
-    subprocess.run(['ffmpeg', '-r', '30', '-i', f, out_file])
+    subprocess.run(['ffmpeg', '-y', '-r', '30', '-i', f, out_file])
